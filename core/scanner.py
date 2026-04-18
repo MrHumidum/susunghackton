914f49daf15179a9bc13f69570e62fd47d0d8754
MrHumidum <salavatcool5@gmail.com>
2026-04-18 14:52:01 +0200

    scanner excluding dirs

--- core/scanner.py
+++ core/scanner.py
@@ -1,10 +1,15 @@
 from pathlib import Path
 import logging
 
+EXCLUDED_DIRS = {".git", "__pycache__", "node_modules"}
+
 
 def scan(base_path: Path):
     for path in base_path.rglob('*'):
         try:
+            if any(part in EXCLUDED_DIRS for part in path.parts):
+                continue
+
             if path.is_file():
                 yield path
 
