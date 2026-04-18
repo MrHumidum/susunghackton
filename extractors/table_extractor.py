e3f0eca665b120b7fdb24fb4c68b70e97f84b5a9
Никита Боронин <nikitab157@outlook.com>
2026-04-18 17:35:51 +0300

    add functuion table_extractor.py

---
+++ extractors/table_extractor.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from pathlib import Path
+import logging
+
+def extract_table(file_path: Path) -> str:
+    try:
+        suffix = file_path.suffix.lower()
+
+        if suffix == ".csv":
+            df = pd.read_csv(file_path, dtype=str)
+
+        elif suffix == ".json":
+            df = pd.read_json(file_path, dtype=str)
+
+        elif suffix in {".xls", ".xlsx"}:
+            df = pd.read_excel(file_path, dtype=str)
+
+        elif suffix == ".parquet":
+            df = pd.read_parquet(file_path)
+
+        else:
+            return "NO_EXTENSION"
+
+        df = df.astype(str)
+
+        text = "\n".join(df.apply(lambda row: " ".join(row.values), axis=1))
+
+        return text
+
+    except Exception as e:
+        logging.error(f"Ошибка чтения таблицы {file_path}: {e}")
+        return ""
